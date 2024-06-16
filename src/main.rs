32c7dc9207de26cbef5ceb3c4ad7a62ad32819c8
Steve Wang <steve235lab@hotmail.com>
2024-06-16 18:08:27 +0800

    Clean.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use rand::Rng;
-// use std::cmp::Ordering;
-// use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn main() {
@@ -77,29 +75,4 @@ fn main() {
             panic!("you should type y or n!!!");
         }
     }
-
-    // let mut correct_guess = false;
-    // let mut guessed_number: i32 = -1;
-    // while !correct_guess {
-    //     let mut guessed_number_str = String::new();
-    //     std::io::stdin()
-    //         .read_line(&mut guessed_number_str)
-    //         .expect("Cannot read input line from stdin");
-    //     guessed_number = guessed_number_str
-    //         .trim()
-    //         .parse()
-    //         .expect("Input is not an integer");
-    //     match guessed_number.cmp(&random_number) {
-    //         Ordering::Less => println!(
-    //             "You guessed {}, which is smaller than the correct number",
-    //             guessed_number
-    //         ),
-    //         Ordering::Greater => println!(
-    //             "You guessed {}, which is larger than the correct number",
-    //             guessed_number
-    //         ),
-    //         Ordering::Equal => correct_guess = true,
-    //     }
-    // }
-    // println!("You guessed {}, CORRECT! CONGRATS!!", guessed_number);
 }
